7fb51cfcb43d3034864130c517a34c1f46c77269
Yash <yashjariwala120396@gmail.com>
2023-04-27 11:36:11 +0530

    practical

--- Practical/script.js
+++ Practical/script.js
@@ -1,16 +1,16 @@
 let open_btn = document.getElementById("open_bars")
 let nav = document.querySelector(".nav")
 open_btn.addEventListener("click", () => {
-    nav.classList.toggle("show_menu")
+    nav.classList.toggle("p-menu")
 })
 
 function changebg() {
     let header = document.querySelector(".header")
     let current = window.scrollY
     if (current > 0) {
-        header.classList.add("header_fix")
+        header.classList.add("header-fix")
     } else {
-        header.classList.remove("header_fix")
+        header.classList.remove("header-fix")
     }
 }
-window.addEventListener("scroll", changebg)
+window.addEventListener("scroll", changebg(white))
